9bb9be9599379d8744701ad49e0f38a4eff46a64
edgarsilva <edgar.osc@gmail.com>
2014-07-30 18:19:31 -0500

    Added option to adaptor so we can specify a different voice.

--- examples/speech.js
+++ examples/speech.js
@@ -1,7 +1,10 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { name: 'speech', adaptor: 'speech' },
+  // voice for espeak can be specified either in one string or as params for the adaptor.
+  // both connections below will reproduce with the same voice.
+  // connection: { name: 'speech', adaptor: 'speech', language: 'en, gender: 'f', 'voice: '1' },
+  connection: { name: 'speech', adaptor: 'speech', voice: 'en-f3' },
   device: {name: 'mouth', driver: 'speech'},
 
   work: function(my) {
